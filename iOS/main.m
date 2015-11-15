c6e42a8438ed65226cfa43426d1aec4fcf09283e
samhitak <samhita.karnati@gmail.com>
2015-11-15 08:45:05 -0500

    Create main.m

---
+++ iOS/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  WatchOut
+//
+//  Created by Sreela Kodali on 11/15/15.
+//  Copyright © 2015 Sreela Kodali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+#import "PebbleKit/PebbleKit.h"
+#import <Firebase/Firebase.h>
+
+
+
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
