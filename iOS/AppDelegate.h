ac8edc31fc7c997ae097c23e847e3ca202c104dd
samhitak <samhita.karnati@gmail.com>
2015-11-15 08:44:29 -0500

    Create AppDelegate.h

---
+++ iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WatchOut
+//
+//  Created by Sreela Kodali on 11/15/15.
+//  Copyright © 2015 Sreela Kodali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
