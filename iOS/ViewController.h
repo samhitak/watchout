2c701269ccb36e80ad9602b39b82a79b93fd8770
samhitak <samhita.karnati@gmail.com>
2015-11-15 08:45:22 -0500

    Create ViewController.h

---
+++ iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WatchOut
+//
+//  Created by Sreela Kodali on 11/15/15.
+//  Copyright © 2015 Sreela Kodali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+- (IBAction)buttonTapped:(UIButton *)sender;
+
+@end
+
